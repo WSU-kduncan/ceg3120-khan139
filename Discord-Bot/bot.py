a89db17ac0c2d36a8fd276ec42123edec3caf2f0
Ubuntu <khan.139@wright.edu>
2022-09-19 22:22:00 +0000

    AWS add

--- Discord-Bot/bot.py
+++ Discord-Bot/bot.py
@@ -29,25 +29,15 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
+    discord_quotes = [
+        'Hello World!',
+        'Hey!',
+        'Have a good day!',
     ]
 
-    hitchhiker_quotes = [
-        'There is an art, it says, or rather, a knack to flying. The knack lies in learning how to throw yourself at the ground and miss.',
-        'It is a mistake to think you can solve any major problems just with potatoes.',
-        'In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.',
-        'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.',
-    ]
-
-    if message.content == 'towel!':
+    if message.content == 'greeting!':
         #response = random.choice(brooklyn_99_quotes)
-        response = random.choice(hitchhiker_quotes)
+        response = random.choice(discord_quotes)
         await message.channel.send(response)
 
 client.run(TOKEN)
